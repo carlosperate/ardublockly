031460671737c8dd824c343968b0adba03d3561f
q.neutron@gmail.com <q.neutron@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2012-06-01 06:46:10 +0000

    Fix range bug in Python.controls_for. Add missing reserved words.
    git-svn-id: http://blockly.googlecode.com/svn/trunk@194 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- language/python_control.js
+++ language/python_control.js
@@ -59,9 +59,18 @@ Blockly.Python.controls_for = function() {
   var variable0 = Blockly.Python.variableDB_.getName(
       this.getVariableInput(0), Blockly.Variables.NAME_TYPE);
   var argument0 = Blockly.Python.valueToCode_(this, 0, true) || '0';
+  // If starting index is 0, omit it.
+  argument0 = (parseInt(argument0, 10) === 0) ? '' : argument0 + ', ';
   var argument1 = Blockly.Python.valueToCode_(this, 1, true) || '0';
+  if (argument1.match(/^\d+$/)) {
+    // If the index is a naked number, increment it right now.
+    argument1 = parseInt(argument1, 10) + 1;
+  } else {
+    // If the index is dynamic, increment it in code.
+    argument1 += ' + 1';
+  }
   var branch0 = Blockly.Python.statementToCode_(this, 0) || '  pass\n';
-  var code = 'for ' + variable0 + ' in range(' + argument0 + ', ' + 
+  var code = 'for ' + variable0 + ' in range(' + argument0  + 
       argument1 + '):\n' + branch0 + '\n';
   return code;
 };
