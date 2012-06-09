56de51a2defffa5d62b30b0845e2cde76d237943
neil.fraser@gmail.com <neil.fraser@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2012-06-09 08:03:40 +0000

    Make non-latin names slightly less horrific.
    git-svn-id: http://blockly.googlecode.com/svn/trunk@226 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- names.js
+++ names.js
@@ -117,9 +117,9 @@ Blockly.Names.prototype.safeName_ = function(name) {
   if (!name) {
     name = 'unnamed';
   } else {
-    // Unfortunately names in non-latin characters will all be sequences of _s.
-    // TODO: Make friendlier names for non-latin variables.
-    name = name.replace(/[^\w]/g, '_');
+    // Unfortunately names in non-latin characters will look like
+    // _E9_9F_B3_E4_B9_90 which is pretty meaningless.
+    name = encodeURI(name).replace(/[^\w]/g, '_');
     // Most languages don't allow names with leading numbers.
     if ('0123456789'.indexOf(name.charAt(0)) != -1) {
       name = 'my_' + name;
