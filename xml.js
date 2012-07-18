ad4a85a2519dd4635d90a7b9886b3305039b9d50
neil.fraser@gmail.com <neil.fraser@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2012-07-18 20:59:14 +0000

    Fix importing of collapsed blocks from XML.  Issue 60.
    git-svn-id: http://blockly.googlecode.com/svn/trunk@359 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- xml.js
+++ xml.js
@@ -231,21 +231,6 @@ Blockly.Xml.domToBlock_ = function(workspace, xmlBlock) {
   var block = new Blockly.Block(workspace, prototypeName);
   block.initSvg();
 
-  var inline = xmlBlock.getAttribute('inline');
-  if (inline) {
-    block.setInputsInline(inline == 'true');
-  }
-
-  var collapsed = xmlBlock.getAttribute('collapsed');
-  if (collapsed) {
-    block.setCollapsed(collapsed == 'true');
-  }
-
-  var disabled = xmlBlock.getAttribute('disabled');
-  if (disabled) {
-    block.setDisabled(disabled == 'true');
-  }
-
   for (var x = 0, xmlChild; xmlChild = xmlBlock.childNodes[x]; x++) {
     if (xmlChild.nodeType == 3 && xmlChild.data.match(/^\s*$/)) {
       // Extra whitespace between tags does not concern us.
@@ -339,6 +324,22 @@ Blockly.Xml.domToBlock_ = function(workspace, xmlBlock) {
         // Unknown tag; ignore.  Same principle as HTML parsers.
     }
   }
+
+  var inline = xmlBlock.getAttribute('inline');
+  if (inline) {
+    block.setInputsInline(inline == 'true');
+  }
+
+  var collapsed = xmlBlock.getAttribute('collapsed');
+  if (collapsed) {
+    block.setCollapsed(collapsed == 'true');
+  }
+
+  var disabled = xmlBlock.getAttribute('disabled');
+  if (disabled) {
+    block.setDisabled(disabled == 'true');
+  }
+
   block.render();
   return block;
 };
