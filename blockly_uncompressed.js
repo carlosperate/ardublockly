a3045b96ae5662ff011b642386c60d94a25f7c27
neil.fraser@gmail.com <neil.fraser@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-03-21 22:35:17 +0000

    Adding Serbian Review URL: https://codereview.appspot.com/7865047
    git-svn-id: http://blockly.googlecode.com/svn/trunk@708 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- blockly_uncompressed.js
+++ blockly_uncompressed.js
@@ -961,5 +961,6 @@ delete window.BLOCKLY_DIR;
 delete window.BLOCKLY_BOOT;
 };
 
-document.write('<script type="text/javascript" src="' + window.BLOCKLY_DIR + '/../closure-library-read-only/closure/goog/base.js"></script>');
+document.write('<script type="text/javascript" src="' + window.BLOCKLY_DIR +
+    '/../closure-library-read-only/closure/goog/base.js"></script>');
 document.write('<script type="text/javascript">window.BLOCKLY_BOOT()</script>');
