5466076cf6846c9813d4c2d40a4d2155933a84c3
spertus@google.com <spertus@google.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-07-17 20:45:12 +0000

    Removed reference to apps/*.js from code and graph applications. Removed apps/{de,hu,vi}.js. Added file (maze/template.js) left out of previous review.
    R=neil.fraser@gmail.com
    
    Review URL: https://codereview.appspot.com/11475043
    
    git-svn-id: http://blockly.googlecode.com/svn/trunk@1105 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- apps/vi.js
+++
@@ -1,9 +0,0 @@
-// This file was automatically generated from template.soy.
-// Please don't edit this file by hand.
-
-if (typeof BlocklyCommonMessages == 'undefined') { var BlocklyCommonMessages = {}; }
-
-
-BlocklyCommonMessages.messages = function(opt_data, opt_ignored) {
-  return '<div id="COMMON_MSG" style="display: none"><span id="blocklyMessage">Blockly</span><span id="subtitle">a visual programming environment.</span><span id="httpRequestError">Hoạt động bị trục trặc, không thực hiện được yêu cầu của bạn.</span><span id="linkAlert">Dùng liên kết này để chia sẽ chương trình của bạn:\n\n%1</span><span id="hashError">Không tìm thấy chương trình được lưu ở \'%1\'.</span><span id="xmlError">Không mở được chương trình của bạn.  Có thể nó nằm trong một phiên bản khác của Blockly?</span></div>';
-};
