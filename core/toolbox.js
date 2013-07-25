f0e8c8c8e3038592f8835f9dc8ddf94e45ce0ae6
tobyk100 <tobyk100@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-07-25 21:31:57 +0000

    The constructor for Toolbox.TreeControl was a bad place to attach events since the DOM is not ready. Instead I overrode enterDocument with a call to the superclass, then attaching touchEvents. Also, there was a really interesting bug with attaching the touch handler. The onMouseDown method often took more than the time (I think 300ms) that the browser waits after a touch event before firing a mouse event. So before we could return from our handler (and register e.preventDefault) the mouse event was fired and queued on the execution thread. I confirmed this behavior with a test script which attached mouse and touch events to a div When the callback for touch spun for 1 second the mouse events got called, even though I called e.preventDefault(). When the 1 second sleep was removed the mouse events didn't fire.
    R=neil.fraser@gmail.com
    
    Review URL: https://codereview.appspot.com/11855043
    
    git-svn-id: http://blockly.googlecode.com/svn/trunk@1150 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- core/toolbox.js
+++ core/toolbox.js
@@ -206,25 +206,37 @@ Blockly.Toolbox.clearSelection = function() {
  */
 Blockly.Toolbox.TreeControl = function(html, opt_config, opt_domHelper) {
   goog.ui.tree.TreeControl.call(this, html, opt_config, opt_domHelper);
+};
+goog.inherits(Blockly.Toolbox.TreeControl, goog.ui.tree.TreeControl);
+
+/**
+ * Adds touch handling to TreeControl.
+ * @override
+ */
+Blockly.Toolbox.TreeControl.prototype.enterDocument = function() {
+  Blockly.Toolbox.TreeControl.superClass_.enterDocument.call(this);
 
   // Add touch handler.
   if (goog.events.BrowserFeature.TOUCH_ENABLED) {
     var el = this.getElement();
-    Blockly.bindEvent_(el, goog.events.EventType.TOUCHSTART,
+    Blockly.bindEvent_(el, goog.events.EventType.TOUCHSTART, this,
         this.handleTouchEvent_);
   }
 };
-goog.inherits(Blockly.Toolbox.TreeControl, goog.ui.tree.TreeControl);
-
 /**
  * Handles touch events.
  * @param {!goog.events.BrowserEvent} e The browser event.
  * @private
  */
 Blockly.Toolbox.TreeControl.prototype.handleTouchEvent_ = function(e) {
+  e.preventDefault();
   var node = this.getNodeFromEvent_(e);
   if (node && e.type === goog.events.EventType.TOUCHSTART) {
-    node.onMouseDown(e);  // Same behavior for click and touch.
+    // Fire asynchronously since onMouseDown takes long enough that the browser
+    // would fire the default mouse event before this method returns.
+    window.setTimeout(function() {
+      node.onMouseDown(e);  // Same behavior for click and touch.
+    }, 1);
   }
 };
 
