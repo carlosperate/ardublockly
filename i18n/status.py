ee0f9b834ea5bdea42cfa277c387a4375135df10
spertus@google.com <spertus@google.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-09-16 23:05:26 +0000

    Improvements to status.py: - Changes internal data representation. - Provides links to information about each language. - Use string.format() instead of string concatenation in places. - Minor other improvements in output appearance.
    R=neil.fraser@gmail.com
    
    Review URL: https://codereview.appspot.com/13411047
    
    git-svn-id: http://blockly.googlecode.com/svn/trunk@1347 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- i18n/status.py
+++ i18n/status.py
@@ -28,8 +28,10 @@ import json
 # Bogus language name representing all messages defined.
 TOTAL = 'qqq'
 
-# The parsed command-line arguments will be stored here.
-global args
+# List of key prefixes, which are app names, except for 'Apps', which
+# has common messages.  It is included here for convenience.
+APPS = ['Apps', 'Code', 'Graph', 'Maze', 'Plane', 'Puzzle', 'Turtle']
+
 
 def get_prefix(s):
   """Gets the portion of a string before the first period.
@@ -44,26 +46,36 @@ def get_prefix(s):
   return s.split('.')[0]
 
 
-def get_prefix_counts(filename):
-  """Builds a dictionary mapping key prefixes to their counts.
+def get_json(filename):
+  """Reads in a JSON file.
 
   Args:
       filename: The name of a JSON file.
 
   Returns:
-      A dictionary mapping each key prefix to its count.
+      A dictionary mapping each JSON key to its value.
   """
-  prefixes = {}
-  f = open(filename)
-  keys = json.load(f)
-  for key in keys:
-    prefix = key.split('.')[0]
-    prefixes[prefix] = prefixes.get(prefix, 0) + 1
-  f.close()
-  return prefixes
+  try:
+    f = open(filename)
+    return json.load(f)
+  finally:
+    f.close()
 
 
-def output_as_html(prefix_counts):
+def get_prefix_count(prefix, arr):
+  """Counts how many strings in the array start with the prefix.
+
+  Args:
+      prefix: The prefix string.
+      arr: An array of strings.
+  Returns:
+      The number of strings in arr starting with prefix.
+  """
+  # This code was chosen for its elegance not its efficiency.
+  return len([elt for elt in arr if elt.startswith(prefix)])
+
+
+def output_as_html(messages, verbose):
   """Outputs the given prefix counts and percentages as HTML.
 
   Specifically, a sortable HTML table is produced, where the app names
@@ -71,13 +83,16 @@ def output_as_html(prefix_counts):
   are color-coded based on the percent completeness.
 
   Args:
-      prefix_counts: A dictionary of dictionaries, where keys to the outer
-          dictionary are ISO 639 language names (e.g., 'pt') or the special
-          string TOTAL, used to indicate the total number of messages.  Keys
-          of the inner dictionaries are prefixes (app names or "apps"), and
-          values are their count for the given language.
+      messages: A dictionary of dictionaries, where the outer keys are language
+          codes used by translatewiki (generally, ISO 639 language codes) or
+          the string TOTAL, used to indicate the total set of messages.  The
+          inner dictionary makes message keys to values in that language.
+      verbose: Whether to list missing keys.
   """
-  def generateNumberAsPercent(num, total):
+  def generate_language_url(lang):
+    return 'https://translatewiki.net/wiki/Special:SupportedLanguages#' + lang
+
+  def generate_number_as_percent(num, total, tag):
     percent = num * 100 / total
     if percent == 100:
       color = 'green'
@@ -87,86 +102,119 @@ def output_as_html(prefix_counts):
       color = 'black'
     else:
       color = 'gray'
-    return ('<font color=' + color + '>' + str(num) + ' (' + str(percent) +
-            '%)</font>')
+    s = '<font color={0}>{1} ({2}%)</font>'.format(color, num, percent)
+    if verbose and percent < 100:
+      return '<a href="#{0}">{1}'.format(tag, s)
+    else:
+      return s
 
-  apps = prefix_counts[TOTAL].keys()
+  print('<head><title>Blockly app translation status</title></head><body>')
   print("<SCRIPT LANGUAGE='JavaScript1.2' SRC='https://neil.fraser.name/"
         "software/tablesort/tablesort-min.js'></SCRIPT>")
   print('<table cellspacing=5><thead><tr>')
   print('<th class=nocase>Language</th><th class=num>' +
-        '</th><th class=num>'.join(apps) + '</th></tr></thead><tbody>')
-  for lang in prefix_counts:
+        '</th><th class=num>'.join(APPS) + '</th></tr></thead><tbody>')
+  for lang in messages:
     if lang != TOTAL:
-      print('<tr><td>' + lang + '</td>')
-      for app in apps:
+      print('<tr><td><a href="{1}">{0}</a></td>'.format(
+          lang, generate_language_url(lang)))
+      for app in APPS:
         print '<td>'
-        print generateNumberAsPercent(prefix_counts[lang].get(app, 0),
-                                      prefix_counts[TOTAL][app])
+        print(generate_number_as_percent(
+            get_prefix_count(app, messages[lang]),
+            get_prefix_count(app, messages[TOTAL]),
+            (lang + app)))
         print '</td>'
       print('</tr>')
   print('</tbody><tfoot><tr><td>ALL</td><td>')
-  print('</td><td>'.join([str(prefix_counts[TOTAL][app]) for app in apps]))
+  print('</td><td>'.join([str(get_prefix_count(app, TOTAL)) for app in APPS]))
   print('</td></tr></tfoot></table>')
 
+  if verbose:
+    for lang in messages:
+      if lang != TOTAL:
+        for app in APPS:
+          if (get_prefix_count(app, messages[lang]) <
+              get_prefix_count(app, messages[TOTAL])):
+            print('<div id={0}{1}><strong>{1} (<a href="{2}">{0}</a>)'.
+                  format(lang, app, generate_language_url(lang)))
+            print('</strong> missing: ')
+            print(', '.join(
+                [key for key in messages[TOTAL] if
+                 key.startswith(app) and key not in messages[lang]]))
+            print('<br><br></div>')
+  print('</body>')
 
-def output_as_text(prefix_counts):
+
+def output_as_text(messages, verbose):
   """Outputs the given prefix counts and percentages as text.
 
   Args:
-      prefix_counts: A dictionary of dictionaries, where keys to the outer
-          dictionary are ISO 639 language names (e.g., 'pt') or the special
-          string TOTAL, used to indicate the total number of messages.  Keys
-          of the inner dictionaries are prefixes (app names or "apps"), and
-          values are their count for the given language.
+      messages: A dictionary of dictionaries, where the outer keys are language
+          codes used by translatewiki (generally, ISO 639 language codes) or
+          the string TOTAL, used to indicate the total set of messages.  The
+          inner dictionary makes message keys to values in that language.
+      verbose: Whether to list missing keys.
   """
   def generate_number_as_percent(num, total):
-    return ('{0} ({1}%)'.format(num, num * 100 / total))
+    return '{0} ({1}%)'.format(num, num * 100 / total)
   MAX_WIDTH = len('999 (100%)') + 1
   FIELD_STRING = '{0: <' + str(MAX_WIDTH) + '}'
-  apps = prefix_counts[TOTAL].keys()
   print(FIELD_STRING.format('Language') + ''.join(
-      [FIELD_STRING.format(app) for app in apps]))
-  print(('-' * (MAX_WIDTH - 1) + ' ') * (len(apps) + 1))
-  for lang in prefix_counts:
+      [FIELD_STRING.format(app) for app in APPS]))
+  print(('-' * (MAX_WIDTH - 1) + ' ') * (len(APPS) + 1))
+  for lang in messages:
     if lang != TOTAL:
       print(FIELD_STRING.format(lang) +
             ''.join([FIELD_STRING.format(generate_number_as_percent(
-                prefix_counts[lang].get(app, 0),
-                prefix_counts[TOTAL][app])) for app in apps]))
+                get_prefix_count(app, messages[lang]),
+                get_prefix_count(app, messages[TOTAL])))
+                     for app in APPS]))
   print(FIELD_STRING.format(TOTAL) +
         ''.join(
-            [FIELD_STRING.format(prefix_counts[TOTAL][app])
-             for app in apps]))
+            [FIELD_STRING.format(get_prefix_count(app, messages[TOTAL]))
+             for app in APPS]))
+  if verbose:
+    for lang in messages:
+      if lang != TOTAL:
+        for app in APPS:
+          missing = [key for key in messages[TOTAL]
+                     if key.startswith(app) and key not in messages[lang]]
+          print('{0} {1}: Missing: {2}'.format(
+              app.upper(), lang, (', '.join(missing) if missing else 'none')))
 
 
 def main():
   """Processes input files and outputs results in specified format.
   """
   # Argument parsing.
-  parser = argparse.ArgumentParser(description='Compare JSON files.')
+  parser = argparse.ArgumentParser(
+      description='Display translation status by app and language.')
   parser.add_argument('--key_file', default='keys.json',
                       help='file with complete list of keys.')
   parser.add_argument('--output', default='text', choices=['text', 'html'],
                       help='output format')
+  parser.add_argument('--verbose', action='store_true', default=False)
+  parser.add_argument('--app', default=None, choices=APPS,
+                      help='If set, only consider the specified app (prefix).')
   parser.add_argument('lang_files', nargs='+',
                       help='names of JSON files to examine')
   args = parser.parse_args()
 
-  # Read in files, building up prefix counts.
-  prefix_counts = {}  # A dictionary of dictionaries.
-  prefix_counts[TOTAL] = get_prefix_counts(args.key_file)
+  # Read in JSON files.
+  messages = {}  # A dictionary of dictionaries.
+  messages[TOTAL] = get_json(args.key_file)
   for lang_file in args.lang_files:
     prefix = get_prefix(lang_file)
     # Skip non-language files.
     if prefix not in ['qqq', 'keys']:
-      prefix_counts[prefix] = get_prefix_counts(lang_file)
+      messages[prefix] = get_json(lang_file)
 
   # Output results.
   if args.output == 'text':
-    output_as_text(prefix_counts)
+    output_as_text(messages, args.verbose)
   elif args.output == 'html':
-    output_as_html(prefix_counts)
+    output_as_html(messages, args.verbose)
   else:
     print('No output?!')
 
