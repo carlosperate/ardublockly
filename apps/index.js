a9d3c2a9ae12ae44bc6e12ef8a5804abd912236e
spertus@google.com <spertus@google.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2013-11-04 21:03:21 +0000

    Caused apps/index.html to be automatically generated, in preparation for i18n.
    Automatically generated files to be added before submission.
    
    R=neil.fraser@gmail.com
    
    Review URL: https://codereview.appspot.com/20080043
    
    git-svn-id: http://blockly.googlecode.com/svn/trunk@1474 c400ca83-b69d-9dd7-9705-49c6b8615e23

---
+++ apps/index.js
@@ -0,0 +1,33 @@
+/**
+ * Blockly Apps: Index
+ *
+ * Copyright 2012 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview JavaScript for Blockly's application index.
+ * @author ellen.spertus@gmail.com (Ellen Spertus)
+ */
+'use strict';
+
+var BlocklyAppsIndex = {};
+
+// Supported languages.
+BlocklyApps.LANGUAGES = ['en'];
+BlocklyApps.LANG = BlocklyApps.getLang();
+
+document.write('<script type="text/javascript" src="generated/' +
+               BlocklyApps.LANG + '.js"></script>\n');
