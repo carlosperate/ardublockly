1c97b58e52e08820e0e47f3e1c69b3e41673602a
neil.fraser@gmail.com <neil.fraser@gmail.com@c400ca83-b69d-9dd7-9705-49c6b8615e23>
2014-08-19 00:47:04 +0000

    Fix sounds on iOS.
    git-svn-id: http://blockly.googlecode.com/svn/trunk@1757 c400ca83-b69d-9dd7-9705-49c6b8615e23

--- core/blockly.js
+++ core/blockly.js
@@ -581,6 +581,11 @@ Blockly.preloadAudio_ = function() {
     sound.volume = .01;
     sound.play();
     sound.pause();
+    // iOS can only process one sound at a time.  Trying to load more than one
+    // corrupts the earlier ones.  Just load one and leave the others uncached.
+    if (goog.userAgent.IPAD || goog.userAgent.IPHONE) {
+      break;
+    }
   }
 };
 
