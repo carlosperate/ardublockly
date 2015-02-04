979541046a93924ffdfc5dc086d75548bbfaf4d3
carlosperate <carlosperate@users.noreply.github.com>
2015-02-04 01:56:12 +0000

    Removing list and colour from Arduino app.

--- generators/arduino/colour.js
+++ generators/arduino/colour.js
@@ -1,26 +1,12 @@
 /**
- * @license
- * Visual Blocks Language
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright 2012 Google Inc.
- * https://blockly.googlecode.com/
+ * @fileoverview Generating Arduino code for colour blocks.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * TODO: These blocks do not really serve a purpose for Arduino code.
  */
 
-/**
- * @fileoverview Generating Arduino for colour blocks.
- */
 'use strict';
 
 goog.provide('Blockly.Arduino.colour');
--- generators/arduino/lists.js
+++ generators/arduino/lists.js
@@ -1,25 +1,11 @@
 /**
- * @license
- * Visual Blocks Language
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright 2012 Google Inc.
- * https://blockly.googlecode.com/
+ * @fileoverview Generating Arduino code for list blocks.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Generating Arduino for list blocks.
+ * TODO: A lot of this can be converted to arrays code by creating functions to
+ *       replicate this kind of behavior.
  */
 'use strict';
 
