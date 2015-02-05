a82007ee979facc661a1479bd38f602f21fdbec0
carlosperate <carlosperate@users.noreply.github.com>
2015-02-05 16:44:55 +0000

    Updated servo blocks and arduino code generator.

---
+++ blocks/arduino/servo.js
@@ -0,0 +1,56 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @fileoverview Arduino blocks for the Servo library.
+ *               The Arduino Servo functions can be found in
+ *               http://arduino.cc/en/reference/servo
+ *
+ * TODO: Add angle selector instead of block input.
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.servo');
+
+goog.require('Blockly.Arduino');
+
+
+Blockly.Blocks.servo.HUE = 50;
+
+Blockly.Blocks['servo_write'] = {
+  /**
+   * Block for writing an angle value into a servo PWM pin
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/ServoWrite');
+    this.setColour(Blockly.Blocks.servo.HUE);
+    this.appendDummyInput('')
+        .appendField('Set SERVO from Pin')
+        .appendField(new Blockly.FieldDropdown(profile.default.pwm), 'SERVO_PIN');
+    this.setInputsInline(false);
+    this.appendValueInput('SERVO_ANGLE', 'Number')
+        .setCheck('Number')
+        .appendField("to");
+    this.appendDummyInput('')
+        .appendField('Degrees (0-180)');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('Set a Servo to an specified angle');
+  }
+};
+
+Blockly.Blocks['servo_read'] = {
+  /**
+   * Block for reading an angle value of a servo PWM pin
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/ServoRead');
+    this.setColour(Blockly.Blocks.servo.HUE);
+    this.appendDummyInput('')
+        .appendField('Read SERVO from PIN#')
+        .appendField(new Blockly.FieldDropdown(profile.default.pwm), 'SERVO_PIN');
+    this.setOutput(true, 'Number');
+    this.setTooltip('Read a Servo angle');
+  }
+};
--- generators/arduino/servo.js
+++ generators/arduino/servo.js
@@ -1,6 +1,12 @@
 /**
- * @fileoverview Blocks extension for Arduino Servo library.
- * The Arduino Servo functions syntax can be found in http://arduino.cc/en/reference/servo
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @fileoverview Arduino code generator for the Servo library blocks.
+ *               The Arduino Servo functions syntax can be found in
+ *               http://arduino.cc/en/reference/servo
+ *
+ * TODO: If angle selector added to blocks edit code here.
  */
 'use strict';
 
@@ -8,54 +14,36 @@ goog.provide('Blockly.Arduino.servo');
 
 goog.require('Blockly.Arduino');
 
-/**
- * Description
- */
-Blockly.Blocks['servo_write'] = {
-  init: function() {
-    this.setHelpUrl('http://arduino.cc/en/Reference/ServoWrite');
-    this.setColour(50);
-    this.appendDummyInput('')
-        .appendField('Set SERVO from Pin')
-        .appendField(new Blockly.FieldDropdown(profile.default.pwm), 'SERVO_PIN');
-    this.setInputsInline(false);
-    this.appendValueInput('SERVO_ANGLE', 'Number')
-        .setCheck('Number')
-        .appendField("to");
-    this.appendDummyInput('')
-        .appendField('Degrees (0-180)');
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip('Set a Servo to an specified angle');
-  }
-};
 
 /**
- * Description
+ * Code generator to set an angle (Y) value to a servo PWM pin (X).
+ * Arduino code: #include <Servo.h>
+ *               Servo myServo_X;
+ *               setup { myServo_X.attach(X); }
+ *               loop  { myServo_X.write(Y);  } 
  */
 Blockly.Arduino['servo_write'] = function(block) {
-  var pin_key = block.getFieldValue('SERVO_PIN');
-  var pin_type = profile.default.pin_types.SERVO;
-  var servo_name = 'myServo_' + pin_key;
-  var servo_angle = Blockly.Arduino.valueToCode(block, 'SERVO_ANGLE', Blockly.Arduino.ORDER_ATOMIC) || '90';
-  var code = servo_name + '.write(' + servo_angle + ');\n';
+  var pinKey = block.getFieldValue('SERVO_PIN');
+  var pinType = profile.default.pin_types.SERVO;
+  var servoName = 'myServo_' + pinKey;
+  var servoAngle = Blockly.Arduino.valueToCode(block, 'SERVO_ANGLE', Blockly.Arduino.ORDER_ATOMIC) || '90';
+  var code = servoName + '.write(' + servoAngle + ');\n';
 
   // Maintain the setup regardless of pin conflict, warning should be enough
   Blockly.Arduino.definitions_['define_servo'] = '#include <Servo.h>\n';
-  Blockly.Arduino.definitions_['global_servo_' + pin_key] = 'Servo ' + servo_name + ';';
-  Blockly.Arduino.setups_['setup_servo_' + pin_key] = servo_name + '.attach(' + pin_key + ');';
+  Blockly.Arduino.definitions_['global_servo_' + pinKey] = 'Servo ' + servoName + ';';
+  Blockly.Arduino.setups_['setup_servo_' + pinKey] = servoName + '.attach(' + pinKey + ');';
 
   // If the IO has been configured already set a block warning for the user
-  if (pin_key in Blockly.Arduino.pins_) {
-     if (Blockly.Arduino.pins_[pin_key] != pin_type) {
-       block.setWarningText('Pin alredy used as ' + Blockly.Arduino.pins_[pin_key]);
+  if (pinKey in Blockly.Arduino.pins_) {
+     if (Blockly.Arduino.pins_[pinKey] != pinType) {
+       block.setWarningText('Pin alredy used as ' + Blockly.Arduino.pins_[pinKey]);
      } else {
        block.setWarningText(null);
      }
   } else {
     // First time this IO pin is used, so configure it
-    Blockly.Arduino.pins_[pin_key] = pin_type;
+    Blockly.Arduino.pins_[pinKey] = pinType;
     block.setWarningText(null);
   }
 
@@ -63,44 +51,33 @@ Blockly.Arduino['servo_write'] = function(block) {
 };
 
 /**
- * Description
- */
-Blockly.Blocks['servo_read'] = {
-  init: function() {
-    this.setHelpUrl('http://arduino.cc/en/Reference/ServoRead');
-    this.setColour(50);
-    this.appendDummyInput('')
-        .appendField('Read SERVO from PIN#')
-        .appendField(new Blockly.FieldDropdown(profile.default.pwm), 'SERVO_PIN');
-    this.setOutput(true, 'Number');
-    this.setTooltip('Read a Servo angle');
-  }
-};
-
-/**
- * Description
+ * Code generator to read an angle value from a servo PWM pin (X).
+ * Arduino code: #include <Servo.h>
+ *               Servo myServo_X;
+ *               setup { myServo_X.attach(X); }
+ *               loop  { myServo_X.read();    } 
  */
 Blockly.Arduino['servo_read'] = function(block) {
-  var pin_key = block.getFieldValue('SERVO_PIN');
-  var pin_type = profile.default.pin_types.SERVO;
-  var servo_name = 'myServo_' + pin_key;
-  var code = servo_name + '.read()';
+  var pinKey = block.getFieldValue('SERVO_PIN');
+  var pinType = profile.default.pin_types.SERVO;
+  var servoName = 'myServo_' + pinKey;
+  var code = servoName + '.read()';
 
   // Maintain the setup regardless of pin conflict, warning should be enough
   Blockly.Arduino.definitions_['define_servo'] = '#include <Servo.h>\n';
-  Blockly.Arduino.definitions_['global_servo_' + pin_key] = 'Servo ' + servo_name + ';';
-  Blockly.Arduino.setups_['setup_servo_' + pin_key] = servo_name + '.attach(' + pin_key + ');';
+  Blockly.Arduino.definitions_['global_servo_' + pinKey] = 'Servo ' + servoName + ';';
+  Blockly.Arduino.setups_['setup_servo_' + pinKey] = servoName + '.attach(' + pinKey + ');';
 
   // If the IO has been configured already set a block warning for the user
-  if (pin_key in Blockly.Arduino.pins_) {
-     if (Blockly.Arduino.pins_[pin_key] != pin_type) {
-       block.setWarningText('Pin alredy used as ' + Blockly.Arduino.pins_[pin_key]);
+  if (pinKey in Blockly.Arduino.pins_) {
+     if (Blockly.Arduino.pins_[pinKey] != pinType) {
+       block.setWarningText('Pin alredy used as ' + Blockly.Arduino.pins_[pinKey]);
      } else {
        block.setWarningText(null);
      }
   } else {
     // First time this IO pin is used, so configure it
-    Blockly.Arduino.pins_[pin_key] = pin_type;
+    Blockly.Arduino.pins_[pinKey] = pinType;
     block.setWarningText(null);
   }
 
