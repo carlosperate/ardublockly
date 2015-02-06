3984caedfd5df74e22a1f23f3f33cf31b3d89ad6
carlosperate <carlosperate@users.noreply.github.com>
2015-02-06 17:21:39 +0000

    Updated time blocks and arduino code generator.

---
+++ blocks/arduino/time.js
@@ -0,0 +1,88 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @fileoverview Blocks for Arduino Time functions.
+ *               The arduino built in functions syntax can be found in
+ *               http://arduino.cc/en/Reference/HomePage
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.time');
+
+goog.require('Blockly.Arduino');
+
+
+Blockly.Blocks.time.HUE = 120;
+
+Blockly.Blocks['time_delay'] = {
+  /**
+   * Delay block definition
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/Delay');
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendValueInput("DELAY_TIME_MILI", 'Number')
+        .setCheck('Number')
+        .appendField("wait");
+    this.appendDummyInput()
+        .appendField("milliseconds");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('Wait specific time in milliseconds');
+  }
+};
+
+Blockly.Blocks['time_delaymicros'] = {
+  /**
+   * delayMicroseconds block definition
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/DelayMicroseconds');
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendValueInput("DELAY_TIME_MICRO", 'Number')
+        .setCheck('Number')
+        .appendField("wait");
+    this.appendDummyInput()
+        .appendField("microseconds");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('Wait specific time in microseconds');
+  }
+};
+
+Blockly.Blocks['time_millis'] = {
+  /**
+   * Elapsed time in milliseconds block definition
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/Millis');
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendDummyInput("")
+        .appendField("elapsed Time (milliseconds)");
+    this.setOutput(true, 'Number');
+    this.setTooltip('Returns the number of milliseconds since the Arduino ' +
+                    'board began running the current program.');
+  }
+};
+
+Blockly.Blocks['time_micros'] = {
+  /**
+   * Elapsed time in microseconds block definition
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('http://arduino.cc/en/Reference/Micros');
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendDummyInput("")
+        .appendField("elapsed Time (microseconds)");
+    this.setOutput(true, 'Number');
+    this.setTooltip('Returns the number of microseconds since the Arduino ' +
+                    'board began running the current program.');
+  }
+};
--- generators/arduino/time.js
+++ generators/arduino/time.js
@@ -1,104 +1,60 @@
 /**
- * @fileoverview Blocks extension for Arduino Time functions.
- * The arduino built in functions syntax can be found in http://arduino.cc/en/Reference/HomePage
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @fileoverview Arduino code generator for the Time blocks.
+ *               The arduino built in functions syntax can be found in
+ *               http://arduino.cc/en/Reference/HomePage
  */
 'use strict';
 
+goog.provide('Blockly.Arduino.time');
+
+goog.require('Blockly.Arduino');
+
 
 /**
- * Delay block definition
- */
-Blockly.Blocks['time_delay'] = {
-  init: function() {
-    this.setHelpUrl('http://arduino.cc/en/Reference/Delay');
-    this.setColour(120);
-    this.appendValueInput("DELAY_TIME", 'Number')
-        .appendField("Delay")
-        .setCheck('Number')
-        .appendField("Milliseconds");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip('Delay specific time in milliseconds');
-  }
-};
-
-/**
- * Delay Arduino code generator
+ * Code generator for the delay Arduino block.
+ * Arduino code: loop { delay(X); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
  */
  Blockly.Arduino['time_delay'] = function(block) {
-  var delay_time = Blockly.Arduino.valueToCode(block, 'DELAY_TIME', Blockly.Arduino.ORDER_ATOMIC) || '1000';
-  var code = 'delay(' + delay_time + ');\n';
+  var delayTime = Blockly.Arduino.valueToCode(
+      block, 'DELAY_TIME_MILI', Blockly.Arduino.ORDER_ATOMIC) || '0';
+  var code = 'delay(' + delayTime + ');\n';
   return code;
 };
 
-
 /**
- * delayMicroseconds block definition
- */
-Blockly.Blocks['time_delaymicros'] = {
-  init: function() {
-    this.setHelpUrl('http://arduino.cc/en/Reference/DelayMicroseconds');
-    this.setColour(120);
-    this.appendValueInput("DELAY_TIME_MS", 'Number')
-        .appendField("Delay")
-        .setCheck('Number')
-        .appendField("Microseconds");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip('Delay specific time in microseconds');
-  }
-};
-
-/**
- * delayMicroseconds Arduino code generator
+ * Code generator for the delayMicroseconds block.
+ * Arduino code: loop { delayMicroseconds(X); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
  */
  Blockly.Arduino['time_delaymicros'] = function(block) {
-  var delay_timems = Blockly.Arduino.valueToCode(block, 'DELAY_TIME_MS', Blockly.Arduino.ORDER_ATOMIC) || '1000';
-  var code = 'delayMicroseconds(' + delay_timems + ');\n';
+  var delayTimeMs = Blockly.Arduino.valueToCode(
+      block, 'DELAY_TIME_MICRO', Blockly.Arduino.ORDER_ATOMIC) || '0';
+  var code = 'delayMicroseconds(' + delayTimeMs + ');\n';
   return code;
 };
 
 /**
- * millis block definition
- */
-Blockly.Blocks['time_millis'] = {
-  init: function() {
-    this.setHelpUrl('http://arduino.cc/en/Reference/Millis');
-    this.setColour(120);
-    this.appendDummyInput("")
-        .appendField("Elapsed Time (Milli)");
-    this.setOutput(true, 'Number');
-    this.setTooltip('Returns the number of milliseconds since the Arduino board began running the current program.');
-  }
-};
-
-/**
- * millis Arduino code generator
+ * Code generator for the elapsed time in milliseconds block.
+ * Arduino code: loop { millis() }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {array} Completed code with order of operation.
  */
  Blockly.Arduino['time_millis'] = function(block) {
   var code = 'millis()';
   return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
 
-
 /**
- * micros block definition
- */
-Blockly.Blocks['time_micros'] = {
-  init: function() {
-    this.setHelpUrl('http://arduino.cc/en/Reference/Micros');
-    this.setColour(120);
-    this.appendDummyInput("")
-        .appendField("Elapsed Time (Micro)");
-    this.setOutput(true, 'Number');
-    this.setTooltip('Returns the number of microseconds since the Arduino board began running the current program.');
-  }
-};
-
-/**
- * micros Arduino code generator
+ * Code generator for the elapsed time in microseconds block.
+ * Arduino code: loop { micros() }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {array} Completed code with order of operation.
  */
  Blockly.Arduino['time_micros'] = function(block) {
   var code = 'micros()';
