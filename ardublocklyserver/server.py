867e7b566e1031bed064136a69b72c7725081bbc
carlosperate <carlosperate@users.noreply.github.com>
2015-06-18 02:44:39 +0100

    Start script: improve app localiser and cli args.

--- ardublocklyserver/server.py
+++ ardublocklyserver/server.py
@@ -15,7 +15,7 @@ PORT = 8000
 
 def start_server(document_root):
     """ Start the server with the document root indicated by argument """
-    print('\nSetting HTTP Server Document Root to: \n\t' + document_root + "\n")
+    print('Setting HTTP Server Document Root to: \n\t' + document_root + "\n")
     os.chdir(document_root)
     server_address = (ADDRESS, PORT)
     server = BaseHTTPServer.HTTPServer(
