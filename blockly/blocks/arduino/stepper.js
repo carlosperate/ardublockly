65e821d367ffd45e2d4d07fc7808b9ed6c545071
Alessio <alciregi@gmail.com>
2017-02-06 10:35:24 +0100

    Changed stepper block to add the option to select two or four pins.

--- blockly/blocks/arduino/stepper.js
+++ blockly/blocks/arduino/stepper.js
@@ -31,6 +31,16 @@ Blockly.Blocks['stepper_config'] = {
   init: function() {
     this.setHelpUrl('http://arduino.cc/en/Reference/StepperConstructor');
     this.setColour(Blockly.Blocks.stepper.HUE);
+
+    var NUMBER_OF_PINS =
+      [[Blockly.Msg.ARD_STEPPER_FOUR_PINS, 'FOUR'],
+      [Blockly.Msg.ARD_STEPPER_TWO_PINS, 'TWO']];
+
+    var dropdown = new Blockly.FieldDropdown(NUMBER_OF_PINS, function(option) {
+      var input = (option == "FOUR");
+      this.sourceBlock_.updateShape_(input);
+    });
+
     this.appendDummyInput()
         .appendField(Blockly.Msg.ARD_STEPPER_SETUP)
         .appendField(
@@ -39,7 +49,13 @@ Blockly.Blocks['stepper_config'] = {
                                       true, true, false),
             'STEPPER_NAME')
         .appendField(Blockly.Msg.ARD_STEPPER_MOTOR);
-    this.appendDummyInput()
+
+    this.appendDummyInput('PINS_DROPDOWN')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.ARD_STEPPER_NUMBER_OF_PINS)
+        .appendField(dropdown, "NUMBER_OF_PINS");
+
+    this.appendDummyInput('PINS')
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField(Blockly.Msg.ARD_STEPPER_PIN1)
         .appendField(new Blockly.FieldDropdown(
@@ -47,6 +63,7 @@ Blockly.Blocks['stepper_config'] = {
         .appendField(Blockly.Msg.ARD_STEPPER_PIN2)
         .appendField(new Blockly.FieldDropdown(
             Blockly.Arduino.Boards.selected.digitalPins), 'STEPPER_PIN2');
+
     this.appendValueInput('STEPPER_STEPS')
         .setCheck(Blockly.Types.NUMBER.checkList)
         .setAlign(Blockly.ALIGN_RIGHT)
@@ -57,6 +74,44 @@ Blockly.Blocks['stepper_config'] = {
         .appendField(Blockly.Msg.ARD_STEPPER_SPEED);
     this.setTooltip(Blockly.Msg.ARD_STEPPER_SETUP_TIP);
   },
+
+  domToMutation: function(xmlElement) {
+    var input = (xmlElement.getAttribute('number_of_pins') == 'FOUR');
+    this.updateShape_(input);
+  },
+
+  mutationToDom: function() {
+    var container = document.createElement('mutation');
+    var input = this.getFieldValue('NUMBER_OF_PINS');
+    container.setAttribute("number_of_pins", input);
+    return container;
+  },
+
+  updateShape_: function(option) {
+    var inputExists = this.getFieldValue('STEPPER_PIN3');
+
+    if (option) {
+      // If true, that is FOUR pins is selected
+      if (!inputExists) {
+         this.getInput("PINS")
+            .appendField(Blockly.Msg.ARD_STEPPER_PIN3, "PIN3")
+            .appendField(new Blockly.FieldDropdown(
+                Blockly.Arduino.Boards.selected.digitalPins), 'STEPPER_PIN3')
+            .appendField(Blockly.Msg.ARD_STEPPER_PIN4, "PIN4")
+            .appendField(new Blockly.FieldDropdown(
+                Blockly.Arduino.Boards.selected.digitalPins), 'STEPPER_PIN4');
+      }
+    } else {
+      // If false, that is two pins is selected
+      if (inputExists) {
+        this.getInput("PINS").removeField("STEPPER_PIN3");
+        this.getInput("PINS").removeField("PIN3");
+        this.getInput("PINS").removeField("STEPPER_PIN4");
+        this.getInput("PINS").removeField("PIN4");
+      }
+    }
+  },
+
   /**
    * Updates the content of the the pin related fields.
    * @this Blockly.Block
@@ -66,6 +121,10 @@ Blockly.Blocks['stepper_config'] = {
         this, 'STEPPER_PIN1', 'digitalPins');
     Blockly.Boards.refreshBlockFieldDropdown(
         this, 'STEPPER_PIN2', 'digitalPins');
+    Blockly.Boards.refreshBlockFieldDropdown(
+        this, 'STEPPER_PIN3', 'digitalPins');
+    Blockly.Boards.refreshBlockFieldDropdown(
+        this, 'STEPPER_PIN4', 'digitalPins');
   }
 };
 
