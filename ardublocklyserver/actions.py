5079d7ce72c87a79e6431df9ab52602d57b7c80f
Ishkildin Ruslan <giggles@mail.ru>
2017-03-08 01:08:03 +0500

    roll back changes to actions/py

--- ardublocklyserver/actions.py
+++ ardublocklyserver/actions.py
@@ -120,13 +120,8 @@ def load_arduino_cli(sketch_path=None):
                 cli_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=False)
             out, error = process.communicate()
-            #out = six.u(out)
-            #error = six.u(error)
-            import locale
-            current_locale = locale.getdefaultlocale()[1]
-            out = out.decode(current_locale)
-            error = error.decode(current_locale)
-
+            out = six.u(out)
+            error = six.u(error)
             exit_code = process.returncode
             print('Arduino output:\n%s' % out)
             print('Arduino Error output:\n%s' % error)
