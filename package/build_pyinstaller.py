67324131fd35b0db9862aa43df18b30a8d23deba
carlosperate <carlosperate@users.noreply.github.com>
2017-04-27 00:46:01 +0100

    Move Windows Python excutable build from py2exe to PyInstaller

--- package/build_pyinstaller.py
+++ package/build_pyinstaller.py
@@ -3,22 +3,12 @@
 #
 # Builds the Ardublockly Python portion of the app for Linux or OS X.
 #
-# Copyright (c) 2016 carlosperate https://github.com/carlosperate/
+# Copyright (c) 2017 carlosperate https://github.com/carlosperate/
+# Licensed under the Apache License, Version 2.0 (the "License"):
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# IMPORTANT: This script is designed to be located one directory level under the
-#            project root folder.
+# IMPORTANT: This script is designed to be located one directory level under
+#            the project root folder.
 #
 # This script file uses PyInstaller to create a self contained executable
 # build of the Ardublockly application.
@@ -68,7 +58,7 @@ def get_os():
     """
     valid_os = ["windows", "linux", "mac"]
 
-    print(script_tab + "Checking for command line argument indicated OS:")
+    print(script_tab + "Checking for command line argument indicated OS.")
     if len(sys.argv) > 1:
         if sys.argv[1] in valid_os:
             # Take the first argument and use it as the os
@@ -76,23 +66,23 @@ def get_os():
                   sys.argv[1])
             return "%s" % sys.argv[1]
         else:
-            print(script_tab + "Invalid command line argument found: %s\n" %
-                  sys.argv[1] + script_tab + "Options available: %s" % valid_os)
+            print(script_tab + "Invalid command line argument found: %s" %
+                  sys.argv[1])
+            print(script_tab + "Options available: %s" % valid_os)
 
     print(script_tab + "Valid command line arg not found, checking system.")
 
     os_found = platform.system()
     if os_found == "Windows":
-        raise SystemExit(script_tab + "OS found is: %s\n" % valid_os[0] +
-                         "Exit: This script is not design to run on Windows.")
+        os_found = valid_os[0]
     elif os_found == "Linux":
-        print(script_tab + "OS found is: %s" % valid_os[1])
-        return valid_os[1]
+        os_found = valid_os[1]
     elif os_found == "Darwin":
-        print(script_tab + "OS found is: %s" % valid_os[2])
-        return valid_os[2]
+        os_found = valid_os[2]
     else:
         raise SystemExit("Exit: OS data found is invalid '%s'" % os_found)
+    print(script_tab + "OS found is: %s" % os_found)
+    return os_found
 
 
 def remove_pyinstaller_temps():
@@ -105,8 +95,8 @@ def remove_pyinstaller_temps():
 
 def pyinstaller_build():
     """
-    Launches a subprocess running Python PyInstaller with the spec file from the
-    package folder. Captures the output streams and checks for errors.
+    Launches a subprocess running Python PyInstaller with the spec file from
+    the package folder. Captures the output streams and checks for errors.
     :return: Boolean indicating the success state of the operation.
     """
     process_args = ["pyinstaller",
@@ -114,7 +104,7 @@ def pyinstaller_build():
     print(script_tab + "Command: %s" % process_args)
 
     pyinstaller_process = subprocess.Popen(process_args)
-    std_op, std_err_op = pyinstaller_process.communicate()
+    _, _ = pyinstaller_process.communicate()
 
     if pyinstaller_process.returncode != 0:
         print(script_tab + "ERROR: PyInstaller returned with exit code: %s" %
@@ -132,12 +122,12 @@ def move_executable_folder():
     original_exec_dir = os.path.join(project_root_dir, "dist", spec_coll_name)
     final_exec_dir = os.path.join(project_root_dir, py_exec_folder)
     if os.path.exists(original_exec_dir):
-        print(script_tab + "Moving exec files from %s \n" % original_exec_dir +
-              script_tab + "to %s" % final_exec_dir)
+        print(script_tab + "Moving exec files from %s" % original_exec_dir)
+        print(script_tab + "to %s" % final_exec_dir)
         shutil.move(original_exec_dir, final_exec_dir)
     else:
-        print(script_tab + "ERROR: PyInstaller executable output folder '%s' " %
-              original_exec_dir + "not found!")
+        print(script_tab + "ERROR: PyInstaller executable output folder "
+              "'%s' not found!" % original_exec_dir)
         return False
     return True
 
@@ -149,48 +139,47 @@ def copy_data_files(os_type):
 
 def create_shell_file(os_type):
     """
-    Creates a shell script fil into the project root to be able to easily launch
-    the Ardublockly application.
+    Creates a shell or batch script file into the project root to be able to
+    easily launch the Ardublockly application.
     The Mac OS X build runs directly from clicking the .app folder, so it no
     longer needs a shell script.
     """
-    shell_text = ""
-    shell_location = ""
-
     # The script depends on platform
-    if os_type == "mac":
-        # There is no need for a shell file in Mac OS X
-        print(script_tab + "There is no need to create shell file in Mac OS X.")
-        return
-    elif os_type == "linux":
+    if os_type == "linux":
         shell_text = '#!/bin/bash\n' \
                      'DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )\n' \
                      'echo "[Shell Launch Script] Executing from: $DIR"\n' \
                      './%s' % os.path.join(exec_folder_name, "ardublockly")
         shell_location = os.path.join(project_root_dir, "ardublockly_run.sh")
+    elif os_type == "windows":
+        shell_text = "@echo off\n" + \
+                     "start %s" % os.path.join(exec_folder_name,
+                                               "ardublockly.exe")
+        shell_location = os.path.join(project_root_dir, "ardublockly_run.bat")
     else:
-        # No other OS expected, so just return. This should never happen
+        # macOS doesn't need a shell file, any other OS type is unexpected
+        print(script_tab + "No shell file created.")
         return
 
+    print(script_tab + "Creating shell file into %s" % shell_location)
     try:
-        print(script_tab + "Creating shell file into %s" % shell_location)
-        bash_file = open(shell_location, "w")
-        bash_file.write(shell_text)
-        bash_file.close()
+        with open(shell_location, "w") as bash_file:
+            bash_file.write(shell_text)
     except Exception as e:
         print(script_tab + "%s" % e)
         print(script_tab + "ERROR: Shell file to launch the Ardublockly "
                            "application could not be created.")
+        return
 
-    # Make shell script executable by launching a subprocess
-    process_args = ["chmod", "+x", "%s" % shell_location]
-    print(script_tab + "Command to make executable: %s" % process_args)
-    try:
-        pyinstaller_process = subprocess.Popen(process_args)
-        std_op, std_err_op = pyinstaller_process.communicate()
-    except Exception as e:
-        print(script_tab + "%s" % e)
-        print(script_tab + "ERROR: Could not make Shell file executable.")
+    if os_type == "linux":
+        # Make shell script executable by launching a subprocess
+        process_args = ["chmod", "+x", "%s" % shell_location]
+        print(script_tab + "Command to make executable: %s" % process_args)
+        try:
+            _, _ = subprocess.Popen(process_args).communicate()
+        except Exception as e:
+            print(script_tab + "%s" % e)
+            print(script_tab + "ERROR: Could not make Shell file executable.")
 
 
 def build_ardublockly():
