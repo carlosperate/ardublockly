021504920dc915bcc3cf4ef6161c904060b2f117
Matthias Pfeil <matthias.pfeil@wwu.de>
2018-04-25 13:57:57 +0200

    moved sensebox stuff to own js file

---
+++ ardublockly/sensebox_extensions.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/** Create a namespace for the application. */
+var SenseboxExtension = SenseboxExtension || {};
+
+/** Initialize function for senseBox extensions, to be called on page load. */
+SenseboxExtension.init = function() {
+  var location = window.location;
+  var urlParams = new URLSearchParams(location.search);
+
+  if (location.hostname !== 'localhost') {
+    //TODO hide all features of running ardublockly locally
+  }
+
+  if (urlParams.has('board')) {
+    window.BOARD = urlParams.get('board');
+  } else {
+    window.BOARD = 'sensebox';
+  }
+
+  //TODO hide old and new blocks depending on selected senseBox version
+  switch (window.BOARD) {
+    case 'sensebox':
+      window.EXTENSION = 'hex';
+      break;
+    case 'sensebox-mcu':
+      window.EXTENSION = 'bin';
+      break;
+  }
+  var clipboard = new ClipboardJS('.copy-btn');
+  clipboard.on('success', function(e) {
+    console.log(e);
+    Ardublockly.MaterialToast(Ardublockly.getLocalStr('code_copied'));
+  });
+  clipboard.on('error', function(e) {
+    console.log(e);
+  });
+
+  document.getElementById("button_copy_clipboard").setAttribute("data-tooltip", Ardublockly.getLocalStr('save_to_clipboard'));
+
+  var compile = document.getElementById('button_compile_sketch');
+  compile.addEventListener('click', function () {
+    var sketch = Ardublockly.generateArduino();
+    var data = {
+      "board": window.BOARD,
+      "sketch": sketch
+    };
+    var request = ArdublocklyServer.createRequest();
+    // The data received is JSON, so it needs to be converted into the right
+    // format to be displayed in the page.
+    var onReady = function() {
+      if (request.readyState == 4) {
+        if (request.status == 200) {
+          var dataReceived = null;
+          try {
+            dataReceived = request.responseText;
+            var a = document.createElement('a');
+            document.body.appendChild(a);
+            var blob = new Blob([dataReceived]);
+            a.href = window.URL.createObjectURL(blob);
+            a.download = document.getElementById('sketch_name').value +'.'+window.EXTENSION;
+            a.click();
+            document.body.removeChild(a);
+          } catch(e) {
+            throw e;
+          }
+        } else {
+          return null;
+        }
+      }
+    };
+    try {
+      request.open('POST', 'https://compiler.sensebox.de/compile', true);
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.onreadystatechange = onReady;
+      request.send(JSON.stringify(data));
+    } catch (e) {
+      console.log('Error: ', e);
+      throw e;
+    }
+  });
+};
