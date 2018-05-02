0a0af7dd72cef485d79d056689da07826b3435bb
Björn Guntermann <bguntermann@gmail.com>
2018-05-02 11:06:10 +0200

    Added german UI translation #5
    Deutsche Übersetzung der GUI

--- ardublockly/msg/de.js
+++ ardublockly/msg/de.js
@@ -2,104 +2,104 @@ var Ardublockly = Ardublockly || {};
 Ardublockly.LOCALISED_TEXT = {
   translationLanguage: "Deutsch",
   title: "Ardublockly",
-  blocks: "Blocks",
+  blocks: "Blöcke",
   /* Menu */
   open: "Öffnen",
-  save: "Save",
-  deleteAll: "Delete All",
-  settings: "Settings",
-  documentation: "Documentation",
-  reportBug: "Report Bug",
-  examples: "Examples",
+  save: "Speichern",
+  deleteAll: "Alles löschen",
+  settings: "Einstellungen",
+  documentation: "Dokumentation",
+  reportBug: "Fehler melden",
+  examples: "Beispiele",
   /* Settings */
-  compilerLocation: "Compiler Location",
-  compilerLocationDefault: "Compiler Location unknown",
-  sketchFolder: "Sketch Folder",
-  sketchFolderDefault: "Sketch Folder unknown",
+  compilerLocation: "Compiler Verzeichnis",
+  compilerLocationDefault: "Compiler Verzeichnis unbekannt",
+  sketchFolder: "Sketch Ordner",
+  sketchFolderDefault: "Sketch Ordner unbekannt",
   arduinoBoard: "Arduino Board",
-  arduinoBoardDefault: "Arduino Board unknown",
+  arduinoBoardDefault: "Arduino Board unbekannt",
   comPort: "COM Port",
-  comPortDefault: "COM Port unknown",
-  defaultIdeButton: "Default IDE Button",
-  defaultIdeButtonDefault: "IDE options unknown",
-  language: "Language",
-  languageDefault: "Language unknown",
+  comPortDefault: "COM Port unbekannt",
+  defaultIdeButton: "Standard IDE Schalter",
+  defaultIdeButtonDefault: "IDE Optionen unbekannt",
+  language: "Sprache",
+  languageDefault: "Sprache unbekannt",
   sketchName: "Sketch Name",
   /* Arduino console output */
-  arduinoOpMainTitle: "Arduino IDE output",
-  arduinoOpWaiting: "Waiting for the IDE output...",
-  arduinoOpUploadedTitle: "Successfully Uploaded Sketch",
-  arduinoOpVerifiedTitle: "Successfully Verified Sketch",
-  arduinoOpOpenedTitle: "Sketch opened in IDE",
-  arduinoOpOpenedBody: "The sketch should be loaded in the Arduino IDE.",
-  arduinoOpErrorTitle: "There has been an error",
-  arduinoOpErrorIdContext_0: "No error.",
-  arduinoOpErrorIdContext_1: "Build or Upload failed.",
-  arduinoOpErrorIdContext_2: "Sketch not found.",
-  arduinoOpErrorIdContext_3: "Invalid command line argument.",
-  arduinoOpErrorIdContext_4: "Preference passed to 'get-pref' flag does not exist.",
-  arduinoOpErrorIdContext_5: "Not Clear, but Arduino IDE sometimes errors with this.",
-  arduinoOpErrorIdContext_50: "Unexpected error code from Arduino IDE",
-  arduinoOpErrorIdContext_51: "Could not create sketch file",
-  arduinoOpErrorIdContext_52: "Invalid path to internally created sketch file",
-  arduinoOpErrorIdContext_53: "Unable to find Arduino IDE<br>" +
-                              "The compiler directory has not been set correctly.<br>" +
-                              "Please ensure the path is correct in the Settings.",
-  arduinoOpErrorIdContext_54: "What should we do with the Sketch?<br>" +
-                              "The launch IDE option has not been set.<br>" +
-                              "Please select an IDE option in the Settings.",
-  arduinoOpErrorIdContext_55: "Serial Port unavailable<br>" +
-                              "The Serial Port is not accessible.<br>" +
-                              "Please check if the Arduino is correctly connected to the PC and select the Serial Port in the Settings.",
-  arduinoOpErrorIdContext_56: "Unknown Arduino Board<br>" +
-                              "The Arduino Board has not been set.<br>" +
-                              "Please select the appropriate Arduino Board from the settings.",
-  arduinoOpErrorIdContext_52: "Unexpected server error.",
-  arduinoOpErrorIdContext_64: "Unable to parse sent JSON.",
-  arduinoOpErrorUnknown: "Unexpected error",
+  arduinoOpMainTitle: "Arduino IDE Ausgabe",
+  arduinoOpWaiting: "Warte auf die Arduino IDE Ausgabe...",
+  arduinoOpUploadedTitle: "Sketch erfolgreich übertragen",
+  arduinoOpVerifiedTitle: "Sketch überprüft",
+  arduinoOpOpenedTitle: "Sketch in IDE geöffnet",
+  arduinoOpOpenedBody: "Der Sketch sollte in der IDE geöffnet sein.",
+  arduinoOpErrorTitle: "Es gab einen Fehler",
+  arduinoOpErrorIdContext_0: "Kein Fehler.",
+  arduinoOpErrorIdContext_1: "Kommpilieren oder Übertragung fehlgeschlagen.",
+  arduinoOpErrorIdContext_2: "Sketch nicht gefunden.",
+  arduinoOpErrorIdContext_3: "Ungültiges Kommandozeilen argument.",
+  arduinoOpErrorIdContext_4: "Die hinterlegten Benutzereinstellungen konnten nicht gefunden werden.",
+  arduinoOpErrorIdContext_5: "Wir sind unsicher, aber die Arduino IDE kommt damit manchmal nicht zurecht.",
+  arduinoOpErrorIdContext_50: "Unerwarteter Fehler in der Arduino IDE",
+  arduinoOpErrorIdContext_51: "Sketch-Datei konnte nicht übertragen werden",
+  arduinoOpErrorIdContext_52: "Ungültiges Verzeichnis um den Sketch zu erstellen",
+  arduinoOpErrorIdContext_53: "Arduino IDE konnte nicht gefunden werden<br>" +
+                              "Das Compiler Verzeichnis wurde nicht korrekt ausgewählt.<br>" +
+                              "Bitte überprüfe den Pfad in den Einstellungen.",
+  arduinoOpErrorIdContext_54: "Was sollen wir mit den Sketch machen?<br>" +
+                              "Die Startoptionen der IDE wurden nicht gewählt.<br>" +
+                              "Bitte wähle eine IDE Option in den Einstellungen.",
+  arduinoOpErrorIdContext_55: "Serieller Port ist nicht vorhanden.<br>" +
+                              "Der Serielle Port ist nicht erreichbar.<br>" +
+                              "Bitte überprüfe ob der Mikrocontroller ordnungsgemäß mit dem Computer verbunden ist und wähle den Seriellen Port in den Einstellungen.",
+  arduinoOpErrorIdContext_56: "Unbekanntes Arduino Board<br>" +
+                              "Das Arduino Board wurde nicht festgelegt.<br>" +
+                              "Bitte wähle ein geeignetes Arduino Board aus den Einstellungen.",
+  arduinoOpErrorIdContext_52: "Unerwarteter Serverfehler.",
+  arduinoOpErrorIdContext_64: "Übertragende JSON konnte nicht zerlegt werden",
+  arduinoOpErrorUnknown: "Unerwarteter Fehler",
   /* Modals */
-  noServerTitle: "Ardublockly app not running",
-  noServerTitleBody: "<p>For all the Ardublockly features to be enabled, the Ardublockly desktop application must be running locally on your computer.</p>" +
-                     "<p>If you are using an online version you will not be able to configure the settings nor load the blocks code into an Arduino.</p>" +
-                     "<p>Installation instruction can be found in the <a href=\"https://github.com/carlosperate/ardublockly\">Ardublockly repository</a>.</p>" +
-                     "<p>If you have Ardublockly already installed, make sure the application is running correctly.</p>",
-  noServerNoLangBody: "If the Ardublockly application is not running the language cannot be fully changed.",
-  addBlocksTitle: "Additional Blocks",
+  noServerTitle: "Die Ardublockly-App ist nich gestartet.",
+  noServerTitleBody: "<p>Damit alle Arduino-Funktionen verfügbar sind, muss das Ardublockly Programm lokal auf deinem Computer installiert sein.</p>" +
+                     "<p>Wenn du die Online Version benutzt wird es nicht möglich sein Einstellungen vorzunehmen noch den Blockly-Code auf den Arduino zu übertragen.</p>" +
+                     "<p>Die Installationsanleitung findest du hier: <a href=\"https://github.com/carlosperate/ardublockly\">Ardublockly repository</a>.</p>" +
+                     "<p>Wenn du das Ardublockly Programm schon installiert hast, überprüfe ob es ordnungsgemäß läuft.</p>",
+  noServerNoLangBody: "Solang das Ardublocky Programm nicht läuft kann die Sprache nicht vollständig geändert werden.",
+  addBlocksTitle: "Zusätzliche Blöcke",
   /* Alerts */
-  loadNewBlocksTitle: "Load new blocks?",
-  loadNewBlocksBody: "Loading a new XML file will replace the current blocks from the workspace.<br>" +
-                     "Are you sure you want to proceed?",
-  discardBlocksTitle: "Delete blocks?",
-  discardBlocksBody: "There are %1 blocks on the workspace.<br>" +
-                     "Are you sure you want to delete them?",
-  invalidXmlTitle: "Invalid XML",
-  invalidXmlBody: "The XML file was not successfully parsed into blocks. Please review the XML code and try again.",
+  loadNewBlocksTitle: "Neue Blöcke laden?",
+  loadNewBlocksBody: "Das Laden der neuen XML-Datei ersetzt die aktuellen Blöcke.<br>" +
+                     "Bist du sicher, dass du fortfahren willst?",
+  discardBlocksTitle: "Blöcke löschen?",
+  discardBlocksBody: "Es sind %1 Blöcke auf dem Arbeitsbereich.<br>" +
+                     "Möchtest du diese wirklich löschen?",
+  invalidXmlTitle: "Ungültige XML",
+  invalidXmlBody: "Die XML-Datei konnte nicht in Blöcke zerlegt werden. Bitte überprüfe den XML-Code und versuche es erneut.",
   /* Tooltips */
-  uploadingSketch: "Uploading Sketch into Arduino...",
-  uploadSketch: "Upload Sketch to the Arduino",
-  verifyingSketch: "Verifying Sketch...",
-  verifySketch: "Verify the Sketch",
-  openingSketch: "Opening Sketch in the Arduino IDE...",
-  openSketch: "Open Sketch in IDE",
-  notImplemented: "Function not yet implemented",
+  uploadingSketch: "Sketch wird übertragen...",
+  uploadSketch: "Sketch erfolgreich übertragen!",
+  verifyingSketch: "Sketch überprüfen...",
+  verifySketch: "Sketch erfolgreich überprüft!",
+  openingSketch: "Sketch wird in der Arduino-IDE geöffnet...",
+  openSketch: "Sketch in Arduino IDE geöffnet!",
+  notImplemented: "Diese Funktion ist noch nicht verfügbar",
   /* Prompts */
   ok: "OK",
   okay: "Okay",
-  cancel: "Cancel",
-  return: "Return",
+  cancel: "Abbrechen",
+  return: "Zurück",
   /* Cards */
-  arduinoSourceCode: "Arduino Source Code",
-  blocksXml: "Blocks XML",
+  arduinoSourceCode: "Arduino Quellcode",
+  blocksXml: "XML Blöcke",
   /* Toolbox Categories*/
-  catLogic: "Logic",
-  catLoops: "Loops",
-  catMath: "Math",
+  catLogic: "Logik",
+  catLoops: "Schleifen",
+  catMath: "Mathematik",
   catText: "Text",
-  catVariables: "Variables",
-  catFunctions: "Functions",
-  catInputOutput: "Input/Output",
-  catTime: "Time",
+  catVariables: "Variablen",
+  catFunctions: "Funktionen",
+  catInputOutput: "Eingang/Ausgang",
+  catTime: "Zeit",
   catAudio: "Audio",
-  catMotors: "Motors",
-  catComms: "Comms",
+  catMotors: "Motoren",
+  catComms: "Allgemein",
 };
