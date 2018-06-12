9962e829b5d4d2600d8b1f0e3a35f90f7d57ebe9
Your Name <jasper_buss@web.de>
2018-06-13 00:37:30 +0200

    fixed issues

--- ardublocklyserver/server.py
+++ ardublocklyserver/server.py
@@ -66,7 +66,7 @@ def set_header_no_cache():
 @app.route('/ardublockly')
 def index_redirect():
     """Redirect the server entry point to the Ardublockly front end."""
-    redirect('/ardublockly/landing.html')
+    redirect('/ardublockly/index.html')
 
 
 @app.route('/ardublockly/<file_path:path>')
