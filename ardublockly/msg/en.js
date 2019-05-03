d4c70cf7f79fed517783cd26e33f809de9d98753
Mario <mario.pesch@uni-muenster.de>
2019-05-03 11:05:41 +0200

    fix german block names

--- ardublockly/msg/en.js
+++ ardublockly/msg/en.js
@@ -108,7 +108,7 @@ Ardublockly.LOCALISED_TEXT = {
   catSenseBoxOutput_Web:"Web",
   catSenseBoxOutput_Webserver:"Webserver",
   catSenseBox_Led: "LED",
-  catSenseBox_Wifi: "Wifi",
+  catSenseBox_Wifi: "WiFi",
   catSenseBox_osem: "openSenseMap",
   catLogic: "Logic",
   catLoops: "Loops",
